45cf43b99ab39c74a536612a1b35e2885b7aa071
Azeezjim <azeexjim@gmail.com>
2022-04-23 05:31:27 -1100

    first commit of the day

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import Details from "./componenta/Details/Details";
+
+import { Grid } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Grid
+        container
+        spacing={0}
+        align="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+      </Grid>
     </div>
   );
 }
