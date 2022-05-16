96cc73ab101caa135bbb6ad612f7415cdab89589
Azeezjim <azeexjim@gmail.com>
2022-05-16 02:43:52 -1100

    latest commit of the day

--- src/useTransaction.js
+++ src/useTransaction.js
@@ -7,7 +7,15 @@ const useTransaction = (title) => {
   resetCatigories(); 
   const { transactions } = useContext(ExpenseTrackerContext)
 
-  const selectedCatigories = transactions.filter((t) => t.type === title)
+  const transactionsPerType = transactions.filter((t) => t.type === title)
+  const total = transactionsPerType.reduce((acc, corrval) => acc += corrval.amount, 0 )
+  const catigories = total ==='income' ? incomeCtigories : expenseCatigories;
+
+  console.log({transactionsPerType, total, catigories });
+
+  transactionsPerType.forEach((t) => {
+    const catigory = catigories.find((c) => c.type === t.catigory)
+  })
 }
 
 export default useTransaction
