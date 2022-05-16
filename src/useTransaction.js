ac380f3ebfa66dc8101b5fd925bac6167c48d683
Azeezjim <azeexjim@gmail.com>
2022-05-16 02:25:14 -1100

    latest commit of the day

---
+++ src/useTransaction.js
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { ExpenseTrackerContext } from './context/context';
+
+import { incomeCtigories, expenseCatigories, resetCatigories } from "./constants/catigories ";
+
+const useTransaction = (titles) => {
+  resetCatigories(); 
+  const 
+}
+
+export default useTransaction
