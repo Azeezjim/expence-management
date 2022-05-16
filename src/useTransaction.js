e61b69d3ea1b865cae7790c1bb8265607c31d0e5
Azeezjim <azeexjim@gmail.com>
2022-05-16 02:30:15 -1100

    latest commit of the day

--- src/useTransaction.js
+++ src/useTransaction.js
@@ -3,9 +3,11 @@ import { ExpenseTrackerContext } from './context/context';
 
 import { incomeCtigories, expenseCatigories, resetCatigories } from "./constants/catigories ";
 
-const useTransaction = (titles) => {
+const useTransaction = (title) => {
   resetCatigories(); 
   const { transactions } = useContext(ExpenseTrackerContext)
+
+  const selectedCatigories = transactions.filter((t) => t.type === title)
 }
 
 export default useTransaction
