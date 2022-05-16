f759f008f7b047889e74c2c3017a9072dab44f44
Azeezjim <azeexjim@gmail.com>
2022-05-16 07:49:13 -1100

    latest commit of the day

--- src/componenta/Details/Details.jsx
+++ src/componenta/Details/Details.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Card, CardHeader, CardContent, Typography} from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
+import useTransaction from '../../useTransaction';
 
 // eslint-disable-next-line no-unused-vars
 import useStyles from "./styles"
@@ -8,13 +9,14 @@ import useStyles from "./styles"
 
 const Details = ( { title } ) => {
   const classes = useStyles();
+  const { total, chatData } = useTransaction(title)
 
   return (
     <Card className={title === "Income" ? classes.income : classes.expense}>
       <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">$50</Typography> 
-        {/* <Doughnut data="" /> */}
+        <Typography variant="h5">${total}</Typography> 
+        <Doughnut data={chatData} />
       </CardContent>
     </Card>
   )
--- src/useTransaction.js
+++ src/useTransaction.js
@@ -1,21 +1,35 @@
 import { useContext } from "react";
 import { ExpenseTrackerContext } from './context/context';
 
-import { incomeCtigories, expenseCatigories, resetCatigories } from "./constants/catigories ";
+import { incomeCategories, expenseCategories, resetCategories } from "./constants/catigories ";
 
 const useTransaction = (title) => {
-  resetCatigories(); 
+  resetCategories(); 
   const { transactions } = useContext(ExpenseTrackerContext)
 
   const transactionsPerType = transactions.filter((t) => t.type === title)
   const total = transactionsPerType.reduce((acc, corrval) => acc += corrval.amount, 0 )
-  const catigories = total ==='income' ? incomeCtigories : expenseCatigories;
+  const catigories = total ==='income' ? incomeCategories : expenseCategories;
 
   console.log({transactionsPerType, total, catigories });
 
   transactionsPerType.forEach((t) => {
     const catigory = catigories.find((c) => c.type === t.catigory)
+
+    if(catigory) catigory.amount += t.amount;
   })
+
+  const filteredCatigory = catigories.filter((c) => c.amount > 0); 
+  
+  const chatData ={
+    datasets : [{
+      data: filteredCatigory.map((c) => c.amount),
+      backgroundColor: filteredCatigory.map((c) => c.color)
+    }],
+    labels: filteredCatigory.map((c) => c.type)
+  }
+
+  return { total, chatData   }  
 }
 
 export default useTransaction
